5d565b5bf6e1ce8ff18bb2f6e485ff49632d4d7c
argondev22 <hyoga.js@gmail.com>
2025-09-16 23:33:56 +0000

    refactor: コードの整形と状態遷移関数のロジックを改善しました。入力処理のエラーチェックを追加し、最終状態の表示を整えました。

--- src/finite_automaton.ts
+++ src/finite_automaton.ts
@@ -4,72 +4,89 @@
  * リファレンス: 令和07年 【春期】【秋期】 応用情報技術者 合格教本 p.30
  */
 
-import { getInputFromStdin } from "./utils/input_from_stdin"
+import { getInputFromStdin } from "./utils/input_from_stdin";
 
 /**
  * 状態の有限集合
  */
 enum STATUS {
-    a,
-    b,
-    c,
+  a,
+  b,
+  c,
 }
 
 /**
  * 入力記号の有限集合
  */
-const INPUT = [0, 1]
+const INPUT = [0, 1];
 
 /**
  * 初期状態
  */
-const INIT_STATUS = STATUS.a
+const INIT_STATUS = STATUS.a;
 
 /**
  * 受理状態集合
  */
-const FINAL_STATUS = [STATUS.c]
+const FINAL_STATUS = [STATUS.c];
 
 /**
  * 状態遷移関数
  */
 function transition(status: STATUS, input: string): STATUS {
-    if (status === STATUS.a) {
-        switch (input) {
-            case "0":
-                return STATUS.b
-        }
+  if (status === STATUS.a) {
+    switch (input) {
+      case "0":
+        return STATUS.b;
     }
-    if (status === STATUS.b) {
-        switch (input) {
-            case "0":
-                return STATUS.a
-            case "1":
-                return STATUS.c
-        }
+  }
+  if (status === STATUS.b) {
+    switch (input) {
+      case "0":
+        return STATUS.a;
+      case "1":
+        return STATUS.c;
     }
-    if (status === STATUS.c) {
-        switch (input) {
-            case "0":
-                return STATUS.a
-        }
+  }
+  if (status === STATUS.c) {
+    switch (input) {
+      case "0":
+        return STATUS.a;
     }
-    return status
+  }
+  return status;
+}
+
+function finiteAutomaton(input: string) {
+  let status = INIT_STATUS;
+  for (const i of input) {
+    status = transition(status, i);
+  }
+  return status;
 }
 
 async function main() {
-    const input = await getInputFromStdin("ビット列を入力してください（0と1のみ）: ")
+  const input = await getInputFromStdin(
+    "ビット列を入力してください（0と1のみ）: "
+  );
 
-    let status = INIT_STATUS
-    for (const i of input) {
-        status = transition(status, i)
+  for (const i of input) {
+    if (INPUT.includes(Number(i))) {
+      console.error("入力は0か1のみです");
+      return;
     }
+  }
 
-    if (FINAL_STATUS.includes(status)) {
-        console.log("受理")
-    } else {
-        console.log("不受理")
-    }
+  const finalStatus = finiteAutomaton(input);
+
+  console.table({
+    最終状態: `STATUS.${STATUS[finalStatus]}`,
+    受理状態集合: `[STATUS.${FINAL_STATUS.map((s) => STATUS[s]).join(
+      ", STATUS."
+    )}]`,
+    条件: "0を奇数個含んで1で終わる",
+    判定結果: FINAL_STATUS.includes(finalStatus) ? "受理" : "不受理",
+  });
 }
 
-main()
+main();
