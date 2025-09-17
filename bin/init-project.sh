0a1ce1878289ace941c6620a525d1c8192f460c9
argondev22 <hyoga.js@gmail.com>
2025-09-17 21:59:55 +0900

    cicd: プロジェクト初期化スクリプトを簡素化し、GitHubユーザー名とメールアドレスの入力を削除しました。また、devcontainer設定ファイルとdocker-composeファイルのコピー処理を追加しました。

--- bin/init-project.sh
+++ bin/init-project.sh
@@ -1,41 +1,12 @@
 #!/usr/bin/env bash
 
-echo "Enter your GitHub username (or organization name):"
-read gh_user_name
+
+echo "Starting project initialization..."
+
+cp ./.devcontainer/devcontainer.example.json ./.devcontainer/devcontainer.json
+cp ./app/docker-compose.example.yml ./app/docker-compose.yml
+
+echo "Project initialization complete."
 
 echo ""
-
-echo "Enter your GitHub email address:"
-read gh_email
-
-echo ""
-
-echo "--------------------------------"
-echo "Configuration:"
-echo "GitHub username (organization): $gh_user_name"
-echo "GitHub email address: $gh_email"
-echo "--------------------------------"
-echo ""
-
-echo "Proceed with the above settings? (y/n):"
-read confirmation
-
-echo ""
-
-if [[ $confirmation == "y" || $confirmation == "Y" ]]; then
-    echo "Starting project setup..."
-    cp ./.devcontainer/devcontainer.example.json ./.devcontainer/devcontainer.json
-
-    sed -i "s/Your Name/$gh_user_name/g" ./.devcontainer/devcontainer.json
-    sed -i "s/your.email@example.com/$gh_email/g" ./.devcontainer/devcontainer.json
-
-    cp ./app/docker-compose.example.yml ./app/docker-compose.yml
-
-    echo "Project setup complete."
-
-    echo ""
-    echo "Please start the Dev Container."
-else
-    echo "Operation cancelled."
-    exit 1
-fi
+echo "Please start the Dev Container."
